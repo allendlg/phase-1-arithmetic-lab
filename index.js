52e7bc74a0cff1223ce08bd35ccb7deea38bc3c8
allendlg <allen.delagarza01@gmail.com>
2021-06-29 00:09:36 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+
+const num1 = 8
+const num2 = 7.75
+const num3 = 4
+const num4 = 20
+const set = (4, 8, 7.75, 20)
+
+const multiply = (num1 * num2)
+
+let random = Math.floor(Math.random() * 10)
+
+let mod = (num3 % num1)
+
+let max = Math.max(set);
+
